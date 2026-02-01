7d4c9ca80275f3e14495aece72cc5fbf912c9a7a
Loan <loan.jean@ens.uvsq.fr>
2026-02-01 20:33:43 +0100

    Passage en SSR

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,23 +4,26 @@ import { defineNuxtConfig } from 'nuxt/config'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  srcDir: 'app',
-  runtimeConfig: {
-    public: {
-      apiUrl: 'https://api.beamsocial.com' // Fallback if env not set
-    }
-  },
-  css: [
-	'./app/assets/main.css'
-],
-  vite: {
-    resolve: {
-      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
-    },
-	plugins: [
-		tailwindcss()
-	]
-  }
+	compatibilityDate: '2025-07-15',
+	ssr: true,
+	nitro: {
+		preset: 'cloudflare-pages'
+	},
+	srcDir: 'app',
+	runtimeConfig: {
+		public: {
+			apiUrl: 'https://api.beamsocial.com' // Fallback if env not set
+		}
+	},
+	css: [
+		'./app/assets/main.css'
+	],
+	vite: {
+		resolve: {
+			extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+		},
+		plugins: [
+			tailwindcss()
+		]
+	}
 })
